4f6ed087c70e37c0e198c016c0a32f6669ed2d11
taher-dev <monmoyzx@gmail.com>
2025-02-17 11:15:39 +0600

    Initial commit

---
+++ Lab-1 (Stuffing-Destuffing)/stuffing.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+void stuffing(int arr[], int size)
+{
+    int result[30];
+
+    if (size < 5)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    else
+    {
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while (i < size)
+        {
+            result[j] = arr[i];
+            if (arr[i] == 1)
+            {
+                ++i;
+                ++j;
+                ++count;
+                if (count == 5)
+                {
+                    result[j] = 0;
+                    ++j;
+                    count = 0;
+                }
+            }
+            else
+            {
+                ++i;
+                ++j;
+                count = 0;
+            }
+        }
+        cout << "After Stuffing:\n";
+        for (i = 0; i < j; i++)
+        {
+            cout << result[i] << " ";
+        }
+    }
+}
+
+int main()
+{
+    cout << "Enter arr size: ";
+    int size;
+    cin >> size;
+    int arr[size];
+
+    for (int &x : arr)
+    {
+        cin >> x;
+    }
+
+    stuffing(arr, size);
+
+    return 0;
+}
